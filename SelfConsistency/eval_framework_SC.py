fb2326fd11429f9403e4d9830334151e65b91e82
Gianfranco Pocobelli <116891378+GPocobelli@users.noreply.github.com>
2026-01-06 13:54:06 +0100

    Update eval_framework_SC.py

--- SelfConsistency/eval_framework_SC.py
+++ SelfConsistency/eval_framework_SC.py
@@ -155,7 +155,7 @@ class LocalModel(BaseModel):
             # Tokenizer (local path)
             self.tokenizer = AutoTokenizer.from_pretrained(
                 self.model_name,
-                local_files_only=True
+                local_files_only=False,
             )
 
             model_kwargs = dict(
@@ -197,7 +197,7 @@ class LocalModel(BaseModel):
             # Model (local path)
             self.model = AutoModelForCausalLM.from_pretrained(
                 self.model_name,
-                local_files_only=True,
+                local_files_only=False,
                 **model_kwargs
             )
 
